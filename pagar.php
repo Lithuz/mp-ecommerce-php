baebe49d9831d366a430c01ed75328d48d3c0a11
Angel Lara <aldesigndevs@gmail.com>
2020-11-25 22:11:24 -0800

    Update pagar.php

--- pagar.php
+++ pagar.php
@@ -8,13 +8,13 @@ MercadoPago\SDK::setAccessToken('PROD_ACCESS_TOKEN');
 // Crea un objeto de preferencia
 $preference = new MercadoPago\Preference();
 
-// Crea un ítem en la preferencia
-// $item = new MercadoPago\Item();
-// $item->title = 'Mi producto';
-// $item->quantity = 1;
-// $item->unit_price = 75.56;
-// $preference->items = array($item);
-// $preference->save();
+Crea un ítem en la preferencia
+$item = new MercadoPago\Item();
+$item->title = 'Mi producto';
+$item->quantity = 1;
+$item->unit_price = 75.56;
+$preference->items = array($item);
+$preference->save();
 echo 'PRUEBA';
 ?>
 
